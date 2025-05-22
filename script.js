fe099cda8a5f2af1cb9624217abb945363125752
sunny55g <140750855+sunny55g@users.noreply.github.com>
2025-05-22 18:27:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function handleConnection() {
         return;
     }
 
-    ws = new WebSocket('wss://webrtc-backend-oi3n.onrender.com');
+    ws = new WebSocket('wss://webrtc-backend-wd3d.onrender.com');
 
     ws.onopen = () => {
         ws.send(JSON.stringify({
